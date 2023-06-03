4e11c787255b90448142d3e4a09d5e8d77cfbe00
shubham <shubhamgotiya919@gmail.com>
2023-06-03 15:26:51 +0530

    restart project

--- application/controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php
-// controllers/DashboardController.php
-
-class DashboardController extends CI_Controller {
-    public function dashboard() {
-        $this->load->view('dashboard');
-    }
-}
-?>
--- application/views/demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Controller.php';
-
-// Create an instance of the controller
-$controller = new Controller();
-
-// Call the view method to display a page
-$controller->view('d');
-?>
